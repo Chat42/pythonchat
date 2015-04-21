94d804c7c09e19619ced980cd7454e836116972e
Jasenka Zoric <jasenka.zoric@gmail.com>
2015-04-21 21:39:03 +1000

    New Chat!

--- client.py
+++ client.py
@@ -3,9 +3,9 @@
 
 # chat - client (Tkinter)
 
-limit=80               #amount of lines kept inside the window
-HOST="127.0.0.1"
-PORT=5000
+limit = 80               # amount of lines kept inside the window
+HOST = "127.0.0.1"
+PORT = 5000
 
 import sys
 from string import *
@@ -15,75 +15,83 @@ from Tkinter import *
 import thread
 
 def listening():
- global tekst,limit
- try:
-   while 1:
-     temp=s.recv(1024)+chr(10)
-     tekst.tag_config("a", foreground="#ffff00")
-     tekst.tag_config("b", foreground="#ffffff")
-     if limit%2==0:
-       tekst.insert(END, temp, "b")
-     else:
-       tekst.insert(END, temp, "a")
-     tekst.see(END)
-     limit=limit-1
-     if limit<0:
-       tekst.delete(1.0, 2.0)
- except:
-   return
+    global tekst, limit
+    try:
+        while 1:
+            temp = s.recv(1024)+chr(10)
+            tekst.tag_config("a", foreground="#ffff00")
+            tekst.tag_config("b", foreground="#ffffff")
+            if limit % 2 == 0:
+                tekst.insert(END, temp, "b")
+            else:
+                tekst.insert(END, temp, "a")
+            tekst.see(END)
+            limit -= 1
+            if limit < 0:
+                tekst.delete(1.0, 2.0)
+    except:
+        return
 
-def SayIt(event):
- global s, pole, username, limit
- inf=pole.get()
- temp=inf.encode('utf8')
- pole.delete(0,END)
- try:
-   s.send(temp)
- except:
-   import tkMessageBox
-   tkMessageBox.showerror("Chat", "Сервер недоступен")
-   sys.exit()
- return
+def say_it(event):
+    global s, pole, username, limit
+    inf = pole.get()
+    temp = inf.encode('utf8')
+    pole.delete(0, END)
+    try:
+        s.send(temp)
+    except:
+        import tkMessageBox
+        tkMessageBox.showerror("Chat", "Сервер недоступен")
+        sys.exit()
+    return
 
-try:
- s=socket(AF_INET, SOCK_STREAM)
- s.connect((HOST,PORT))
-except:
- import tkMessageBox
- tkMessageBox.showerror("Chat", "Сервер недоступен")
- sys.exit()
+
 okno = Tk()
 okno.title("Chat")
 photo = PhotoImage(file="fl.gif")
-canv=Canvas(width=500, height=220)
+canv = Canvas(width=500, height=220)
 item = canv.create_image(0, 0, anchor=NW, image=photo)
 import tkFont
-bigfont=tkFont.Font(font=("Arial", 30, "bold"))
-temp="Forgotten Lore\n (приват)"
+bigfont = tkFont.Font(font=("Arial", 30, "bold"))
+temp = "Forgotten Lore\n (приват)"
 item = canv.create_text(200, 50, text=temp, font=bigfont)
 item = canv.create_text(204, 54, text=temp, font=bigfont)
 item = canv.create_text(203, 53, text=temp, font=bigfont, fill="yellow")
 canv.grid()
 pole = Entry(okno)
-pole.bind("<Return>", SayIt)
+pole.bind("<Return>", say_it)
 pole.config(width=82, fg="black", bg="#ffaaee")
 pole.grid()
 import ScrolledText
 tekst = ScrolledText.ScrolledText(height=12)
 tekst.config(fg="black", bg="#aa66bb")
 tekst.grid()
+
 import tkSimpleDialog
-var=tkSimpleDialog.askstring("Chat", "Ваш ник:")
-username=var.encode('utf8')
+HOST = tkSimpleDialog.askstring("Chat", "Введите IP:")
+
+PORT = tkSimpleDialog.askinteger("Chat", "Введите порт:")
+
+var = tkSimpleDialog.askstring("Chat", "Ваш ник:")
+username = var.encode('utf8')
+
+try:
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((HOST, PORT))
+except:
+    import tkMessageBox
+    tkMessageBox.showerror("Chat", "Сервер недоступен")
+    sys.exit()
+
 try:
     strip(username)
 except:
- sys.exit()
-if username=="":
- sys.exit()
+    sys.exit()
+if username == "":
+    sys.exit()
 pole.focus()
 
 s.send(username)
-thread.start_new_thread(listening,())
+thread.start_new_thread(listening, ())
 
 okno.mainloop()
--- server.py
+++ server.py
@@ -9,66 +9,66 @@ from string import *
 from socket import *
 import thread
  
-HOST=""
-PORT=5000
+HOST = ""
+PORT = 5000
  
 try:
- s=socket(AF_INET, SOCK_STREAM)
- s.bind((HOST,PORT))
- s.listen(5)
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((HOST,PORT))
+    s.listen(5)
 except:
- raw_input("Can't setup server!\n")
- sys.exit()
+    raw_input("Can't setup server!\n")
+    sys.exit()
  
 print "Server is ready!"
-print "Listening at: ",s.getsockname()
+print "Listening at: ", s.getsockname()
  
-def listening(conn,addr,username):
- global clients
- while 1:
-   try:
-     temp=conn.recv(1024)
-   except:
-     return
-   dane=username+": "+temp
-   for i in range(0,(len(clients)/3)):
-     try:
-       clients[i*3].send(dane)
-     except:
-       pass
- return
+def listening(conn, addr, username):
+    global clients
+    while 1:
+        try:
+            temp = conn.recv(1024)
+        except:
+            return
+        dane = username + ": " + temp
+        for i in range(0, (len(clients)/3)):
+            try:
+                clients[i*3].send(dane)
+            except:
+                pass
+    return
  
 def manager():
- global s,clients
- while 1:
-   conn,addr=s.accept()
-   username=strip(conn.recv(1024))
-   thread.start_new_thread(listening,(conn,addr,username))
-   old=0
-   for i in range(0,len(clients)/3):
-     if clients[i*3]==conn:
-       old=1
-       break
-   if old==0:
-     clients.append(conn)
-     clients.append(addr)
-     clients.append(username)
- return
+    global s, clients
+    while 1:
+        conn, addr = s.accept()
+        username = strip(conn.recv(1024))
+        thread.start_new_thread(listening, (conn, addr, username))
+        old = 0
+        for i in range(0, len(clients)/3):
+            if clients[i*3] == conn:
+                old = 1
+                break
+        if old == 0:
+            clients.append(conn)
+            clients.append(addr)
+            clients.append(username)
+    return
  
-clients=[]
-thread.start_new_thread(manager,())
+clients = []
+thread.start_new_thread(manager, ())
 while 1:
-   menu="\nCommands:\n\n"
-   menu=menu+"! - shut down\n"
-   menu=menu+"? - status\n"
-   menu=menu+"\n>"
-   result=raw_input(menu)
-   if strip(result)=="!":
-     s.close()
-     sys.exit()
-   if strip(result)=="?":
-     for i in range(0,(len(clients)/3)):
-       try:
-         print clients[i*3+2],"at: ",clients[i*3].getpeername()
-       except:
-         pass
+    menu = "\nCommands:\n\n"
+    menu += "! - shut down\n"
+    menu += "? - status\n"
+    menu += "\n>"
+    result = raw_input(menu)
+    if strip(result) == "!":
+        s.close()
+        sys.exit()
+    if strip(result) == "?":
+        for i in range(0, (len(clients)/3)):
+            try:
+                print clients[i*3+2], "at: ", clients[i*3].getpeername()
+            except:
+                pass
